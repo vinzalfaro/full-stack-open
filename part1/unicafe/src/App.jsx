16ebfa65876c2dcfb631aa4c57fcd2fb924d9227
Vinz <120233304+entropy143@users.noreply.github.com>
2024-02-12 21:50:50 +0800

    Finish exercise 1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -27,6 +27,10 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good * 100 / all
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -37,6 +41,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
