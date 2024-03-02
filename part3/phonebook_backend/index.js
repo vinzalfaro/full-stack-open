deb0f40e9d901a8bd9fe5270ae215d16c4ab3dc7
Vinz <120233304+entropy143@users.noreply.github.com>
2024-03-02 12:14:33 +0800

    Finish exercise 3.6: phonebook backend step6 (handle POST errors)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -66,6 +66,25 @@ app.delete('/api/persons/:id', (request, response) => {
   
 app.post('/api/persons', (request, response) => {
   const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name is missing' 
+    })
+  }
+  
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number is missing' 
+    })
+  }
+
+  const nameExists = persons.some(person => person.name === body.name);
+  if (nameExists) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    });
+  }
 
   const person = {
     id: Math.floor(Math.random() * 1000000000),
