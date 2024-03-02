eaef22203bc3dde5b1fe9298d022d3d6af047470
Vinz <120233304+entropy143@users.noreply.github.com>
2024-03-02 12:07:06 +0800

    Finish exercise 3.5: phonebook backend step5 (POST new entry)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -59,6 +61,21 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
