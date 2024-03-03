a9350c98f3394cef9d7c4bf227a6a8f5ec28fe36
Vinz <120233304+entropy143@users.noreply.github.com>
2024-03-03 18:36:35 +0800

    Finish exercise 3.9: phonebook backend step9 (connect frontend to backend)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,8 +10,9 @@ morgan.token('postData', (req) => {
     }
     return '';
   });
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+app.use(cors())
 app.use(express.json())
 
 let persons = [
