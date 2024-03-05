edd0c93b654c9a4628858dbec3731f72eb5627fa
Vinz <120233304+entropy143@users.noreply.github.com>
2024-03-05 20:31:01 +0800

    Copy mongo.js code into index.js

--- practice/part3/index.js
+++ practice/part3/index.js
@@ -1,35 +1,43 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+const password = process.argv[2]
+const url =
+  `mongodb+srv://fullstackopen:${password}@cluster0.jxtnwqd.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
