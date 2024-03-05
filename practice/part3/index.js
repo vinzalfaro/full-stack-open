94fb1c4ce91401aa96274e42728caf0ee390a710
Vinz <120233304+entropy143@users.noreply.github.com>
2024-03-05 20:26:52 +0800

    Revert "Copy mongo.js code into index.js"
    This reverts commit b149a62f5e8f32e18daac9ac58971cd06b7fc0b3.

--- practice/part3/index.js
+++ practice/part3/index.js
@@ -1,42 +1,35 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-const url =
-  `mongodb+srv://fullstackopen:fsopassword@cluster0.jxtnwqd.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -89,4 +82,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
